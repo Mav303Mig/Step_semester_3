5612fca30904c356851bd2d946989bdd3b4c3056
Mav303Mig <miteshdhillan6786@gmail.com>
2026-08-22 16:26:10 +0530

    Add files via upload

--- PrimeNumberChecker.java
+++ PrimeNumberChecker.java
@@ -2,29 +2,21 @@ import java.util.Scanner;
 
 public class PrimeNumberChecker {
 
-    static void checkPrime(int number) {
-        boolean prime = true;
-
-        for (int i = 2; i <= number / 2; i++) {
-            if (number % i == 0) {
-                prime = false;
-                break;
-            }
-        }
-
-        if (prime) {
-            System.out.println("Prime");
-        } else {
-            System.out.println("Not Prime");
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number: ");
         int number = sc.nextInt();
 
-        checkPrime(number);
+        boolean isPrime = true;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println("Is the number " + number + " a Prime number? " + isPrime);
     }
 }
