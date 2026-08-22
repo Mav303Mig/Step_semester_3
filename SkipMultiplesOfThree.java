b66db479d48efdfb1cc6aef8ef773a426ef3d07d
Mav303Mig <miteshdhillan6786@gmail.com>
2026-08-22 16:16:30 +0530

    Add files via upload

---
+++ SkipMultiplesOfThree.java
@@ -0,0 +1,16 @@
+public class SkipMultiplesOfThree {
+
+    static void printSkippingMultiplesOfThree() {
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printSkippingMultiplesOfThree();
+    }
+}
